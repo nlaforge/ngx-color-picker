b3892f6b344aa84cbba782cafe1385152e7c1ae4
nic laforge <nic.laforge@hpe.com>
2022-12-14 15:43:16 -0500

    Feature: Developer should be able to adjust the height of the color picker box #297 - remove dist

--- dist/lib/lib/cursor/cursor.component.d.ts
+++
@@ -1,6 +0,0 @@
-import * as i0 from "@angular/core";
-export declare class CursorComponent {
-    radius: number;
-    static ɵfac: i0.ɵɵFactoryDeclaration<CursorComponent, never>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<CursorComponent, "lib-cursor", never, { "radius": "radius"; }, {}, never, never, false>;
-}
